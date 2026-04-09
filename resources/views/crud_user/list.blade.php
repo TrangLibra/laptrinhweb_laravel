5932aff4722b0b5c62061b0f73ca7a882a4d4102
Nguyen Xuan Thu Trang <tn6879065@gmail.com>
2026-04-09 11:16:33 +0700

    create file

---
+++ resources/views/crud_user/list.blade.php
@@ -0,0 +1,36 @@
+<head>
+    <title>Create User</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <th>{{ $user->id }}</th>
+                                <th>{{ $user->name }}</th>
+                                <th>{{ $user->email }}</th>
+                                <th>
+                                    <a href="{{ route('user.readUser', ['id' => $user->id]) }}">View</a> |
+                                    <a href="{{ route('user.updateUser', ['id' => $user->id]) }}">Edit</a> |
+                                    <a href="{{ route('user.deleteUser', ['id' => $user->id]) }}">Delete</a>
+                                </th>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
