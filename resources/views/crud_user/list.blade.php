183e7ae40d6fe5ea04404382ef5134e1cd9bff9b
Nguyen Xuan Thu Trang <tn6879065@gmail.com>
2026-04-10 09:41:03 +0700

    update code list

--- resources/views/crud_user/list.blade.php
+++ resources/views/crud_user/list.blade.php
@@ -4,13 +4,21 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-info-subtle">
+<nav class="navbar navbar-expand-lg" style="background-color: #d9edf7;">
     <div class="container">
-        <a class="navbar-brand" href="#">Laravel Training</a>
-        <div>
-            <a class="me-3 text-decoration-none" href="login">Login</a>
-            <a class="text-decoration-none" href="create">Create user</a>
+
+        <!-- Brand -->
+        <a class="navbar-brand fw-semibold" href="{{ route('users.index') }}">
+            Laravel Training
+        </a>
+
+        <!-- Logout -->
+        <div class="ms-auto">
+            <a href="{{ route('signout') }}" class="text-dark text-decoration-none">
+                Logout
+            </a>
         </div>
+
     </div>
 </nav>
 <main class="login-form">
