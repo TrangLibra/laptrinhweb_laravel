9b9078edff0bf2bf51c7e2aa83f5e0e0c25c6c0c
Nguyen Xuan Thu Trang <tn6879065@gmail.com>
2026-04-10 09:25:06 +0700

    update create

--- resources/views/crud_user/create.blade.php
+++ resources/views/crud_user/create.blade.php
@@ -4,6 +4,15 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-info-subtle">
+    <div class="container">
+        <a class="navbar-brand" href="#">Laravel Training</a>
+        <div>
+            <a class="me-3 text-decoration-none" href="login">Login</a>
+            <a class="text-decoration-none" href="create">Create user</a>
+        </div>
+    </div>
+</nav>
 <main class="signup-form">
         <div class="cotainer">
             <div class="row justify-content-center">
